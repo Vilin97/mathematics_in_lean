7be170b18cfa6d7e99f04ca97eadff0f82f91506
Vasily <vasilin97@gmail.com>
2024-03-27 13:46:17 -0700

    Did second exercise.

--- my MIL/C02_Basics/S01_Calculating.lean
+++ my MIL/C02_Basics/S01_Calculating.lean
@@ -12,7 +12,9 @@ example (a b c : ℝ) : c * b * a = b * (a * c) := by
   rw [mul_comm c a]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [← mul_assoc]
+  rw [mul_comm a b]
+  rw [mul_assoc]
 
 -- An example.
 example (a b c : ℝ) : a * b * c = b * c * a := by
